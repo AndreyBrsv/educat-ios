5e3bb8d693c7ce70b4847ca22e209e089daae715
Ilya Mikheev <mikheev.show@gmail.com>
2019-07-12 16:10:10 +0300

    initial commit

---
+++ Educat/Educat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Educat
+//
+//  Created by Ilya Mikheev on 12/07/2019.
+//  Copyright © 2019 Ilya Mikheev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
